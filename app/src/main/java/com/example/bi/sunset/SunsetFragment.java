98e67eec7f4d15bb4c981e060c18787503bb23bf
walkermannu1 <2469377884@qq.com>
2016-09-17 17:23:35 +0800

    Simple Animator.

--- app/src/main/java/com/example/bi/sunset/SunsetFragment.java
+++ app/src/main/java/com/example/bi/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.example.bi.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,11 @@ import android.view.ViewGroup;
  * Created by Guang on 2016/9/17.
  */
 public class SunsetFragment extends Fragment {
+
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -18,6 +24,26 @@ public class SunsetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
 
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();;
+            }
+        });
+
         return view;
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+        heightAnimator.start();
     }
 }
